32a8c5aa1cc8b506d0af4e0738f515eaa646f1de
pankaajadhikarii <pankaajadhikarii@gmail.com>
2025-09-02 22:17:25 +0545

    feat(auth): add RegisterUser and LoginUser models

---
+++ lib/model/auth/login_model.dart
@@ -0,0 +1,23 @@
+class LoginUser {
+  String email;
+  String password;
+
+  LoginUser({
+    required this.email,
+    required this.password,
+  });
+
+  factory LoginUser.fromJson(Map<String, dynamic> json) {
+    return LoginUser(
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+}
---
+++ lib/model/auth/signup_model.dart
@@ -0,0 +1,39 @@
+class RegisterUser {
+  String name;
+  String role;
+  String email;
+  int age;
+  String password;
+  String confirmPassword;
+
+  RegisterUser({
+    required this.name,
+    required this.role,
+    required this.email,
+    required this.age,
+    required this.password,
+    required this.confirmPassword,
+  });
+
+  factory RegisterUser.fromJson(Map<String, dynamic> json) {
+    return RegisterUser(
+      name: json['name'],
+      role: json['role'],
+      email: json['email'],
+      age: json['age'],
+      password: json['password'],
+      confirmPassword: json['confirmPassword'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'role': role,
+      'email': email,
+      'age': age,
+      'password': password,
+      'confirmPassword': confirmPassword,
+    };
+  }
+}
