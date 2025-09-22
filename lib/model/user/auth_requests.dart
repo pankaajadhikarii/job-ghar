edf79ca8557af06c3184f5107ed2f563dbed539c
pankaajadhikarii <pankaajadhikarii@gmail.com>
2025-09-22 22:56:35 +0545

    feat(auth): implement user and authentication models with responses

--- lib/model/user/auth_requests.dart
+++ lib/model/user/auth_requests.dart
@@ -1,19 +1,24 @@
 import 'package:job_ghar/model/user/base_user.dart';
 
-class User extends BaseUser {
+// Register User Model
+class RegisterUser extends BaseUser {
   final int id;
   final String name;
   final String email;
+  final String password;
+  final String? accessToken;
   final String role;
   final int age;
   final bool isProfileComplete;
   final DateTime createdAt;
   final DateTime updatedAt;
 
-  User({
+  RegisterUser({
     required this.id,
     required this.name,
     required this.email,
+    required this.password,
+    this.accessToken,
     required this.role,
     required this.age,
     required this.isProfileComplete,
@@ -21,11 +26,13 @@ class User extends BaseUser {
     required this.updatedAt,
   });
 
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
+  factory RegisterUser.fromJson(Map<String, dynamic> json) {
+    return RegisterUser(
       id: json['id'],
       name: json['name'],
       email: json['email'],
+      password: json['password'],
+      accessToken: json['accessToken'],
       role: json['role'],
       age: json['age'],
       isProfileComplete: json['isProfileComplete'],
@@ -40,11 +47,40 @@ class User extends BaseUser {
       'id': id,
       'name': name,
       'email': email,
+      'password': password,
+      'accessToken': accessToken,
       'role': role,
       'age': age,
       'isProfileComplete': isProfileComplete,
       'createdAt': createdAt.toIso8601String(),
       'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+}
+
+
+// Login User Model
+class LoginUser extends BaseUser {
+  final String accessToken;
+  final String role;
+
+  LoginUser({
+    required this.accessToken,
+    required this.role,
+  });
+
+  factory LoginUser.fromJson(Map<String, dynamic> json) {
+    return LoginUser(
+      accessToken: json['accessToken'],
+      role: json['role'],
+    );
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'accessToken': accessToken,
+      'role': role,
     };
   }
 }
---
+++ lib/model/user/base_user.dart
@@ -0,0 +1,3 @@
+abstract class BaseUser {
+  Map<String, dynamic> toJson();
+}
